3f32dcbacee5f409809c672023145ed920691a9b
hmkim829 <hmkim829@gmail.com>
2023-03-08 22:02:44 +0900

    test_updateCountAndPeriod, test_delete 작성

--- src/main/java/com/fastcampus/pass/repository/packaze/PackageRepository.java
+++ src/main/java/com/fastcampus/pass/repository/packaze/PackageRepository.java
@@ -1,13 +1,20 @@
 package com.fastcampus.pass.repository.packaze;
 
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
+import javax.transaction.Transactional;
 import java.time.LocalDateTime;
 import java.util.List;
 
 public interface PackageRepository extends JpaRepository<PackageEntity, Integer> {
 
     List<PackageEntity> findByCreatedAtAfter(LocalDateTime dateTime, Pageable pageable);
+
+    @Transactional
+    @Modifying
+    @Query(value = "update PackageEntity p set p.count = :count, p.period = :period where p.packageSeq = :packageSeq")
+    int updateCountAndPeriod(Integer packageSeq, int count, int period);
 }
--- src/test/java/com/fastcampus/pass/repository/packaze/PackageRepositoryTest.java
+++ src/test/java/com/fastcampus/pass/repository/packaze/PackageRepositoryTest.java
@@ -1,6 +1,8 @@
 package com.fastcampus.pass.repository.packaze;
 
 import lombok.extern.slf4j.Slf4j;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -55,5 +57,34 @@ class PackageRepositoryTest {
 
         assertEquals(1, pakageEntities.size());
         assertEquals(packageEntity1.getPackageSeq(), pakageEntities.get(0).getPackageSeq());
+    }
+
+    @Test
+    void test_updateCountAndPeriod() {
+
+        PackageEntity packageEntity = new PackageEntity();
+        packageEntity.setPackageName("바디프로필 이벤트 4개월");
+        packageEntity.setPeriod(90);
+        packageRepository.save(packageEntity);
+
+        int updateCount = packageRepository.updateCountAndPeriod(packageEntity.getPackageSeq(), 30, 120);
+        final PackageEntity updatePackageEntity = packageRepository.findById(packageEntity.getPackageSeq()).get();
+
+        assertEquals(1, updateCount);
+        assertEquals(30, updatePackageEntity.getCount());
+        assertEquals(120, updatePackageEntity.getPeriod());
+    }
+
+    @Test
+    void test_delete() {
+
+        PackageEntity packageEntity = new PackageEntity();
+        packageEntity.setPackageName("제거할 이용권");
+        packageEntity.setCount(1);
+        PackageEntity newPakegeEntity = packageRepository.save(packageEntity);
+
+        packageRepository.deleteById(newPakegeEntity.getPackageSeq());
+
+        assertTrue(packageRepository.findById(newPakegeEntity.getPackageSeq()).isEmpty());
     }
 }
