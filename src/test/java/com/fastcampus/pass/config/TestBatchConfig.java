9a17c9ab5eca048b37368bfa97c6ff01b0b74ee9
hmkim829 <hmkim829@colosseum.kr>
2023-03-09 11:29:00 +0900

    불필요한 코드 수정

--- src/test/java/com/fastcampus/pass/config/TestBatchConfig.java
+++ src/test/java/com/fastcampus/pass/config/TestBatchConfig.java
@@ -9,7 +9,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
-@EnableJpaAuditing
 @EnableAutoConfiguration
 @EnableBatchProcessing
 @EntityScan("com.fastcampus.pass.repository")
--- src/test/java/com/fastcampus/pass/repository/packaze/PackageRepositoryTest.java
+++ src/test/java/com/fastcampus/pass/repository/packaze/PackageRepositoryTest.java
@@ -2,6 +2,7 @@ package com.fastcampus.pass.repository.packaze;
 
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
