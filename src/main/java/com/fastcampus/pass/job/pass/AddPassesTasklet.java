3772204eff418238b35572c4cc37e1501ab3c462
hmkim829 <hmkim829@colosseum.kr>
2023-03-09 16:29:11 +0900

    Batch 이용권 일괄 지급

---
+++ src/main/java/com/fastcampus/pass/job/pass/AddPassesTasklet.java
@@ -0,0 +1,67 @@
+package com.fastcampus.pass.job.pass;
+
+import com.fastcampus.pass.repository.pass.BulkPassEntity;
+import com.fastcampus.pass.repository.pass.BulkPassRepository;
+import com.fastcampus.pass.repository.pass.BulkPassStatus;
+import com.fastcampus.pass.repository.pass.PassEntity;
+import com.fastcampus.pass.repository.pass.PassModelMapper;
+import com.fastcampus.pass.repository.pass.PassRepository;
+import com.fastcampus.pass.repository.user.UserGroupMappingEntity;
+import com.fastcampus.pass.repository.user.UserGroupMappingRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Slf4j
+@Component
+public class AddPassesTasklet implements Tasklet {
+
+    private final PassRepository passRepository;
+    private final BulkPassRepository bulkPassRepository;
+    private final UserGroupMappingRepository userGroupMappingRepository;
+
+    public AddPassesTasklet(PassRepository passRepository, BulkPassRepository bulkPassRepository, UserGroupMappingRepository userGroupMappingRepository) {
+        this.passRepository = passRepository;
+        this.bulkPassRepository = bulkPassRepository;
+        this.userGroupMappingRepository = userGroupMappingRepository;
+    }
+
+    @Override
+    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext){
+
+        final LocalDateTime startedAt = LocalDateTime.now().minusDays(1);
+        final List<BulkPassEntity> bulkPassEntities = bulkPassRepository.findByStatusAndStartedAtGreaterThan(BulkPassStatus.READY, startedAt);
+
+        int count = 0;
+        for (BulkPassEntity bulkPassEntity : bulkPassEntities) {
+            final List<String> userIds = userGroupMappingRepository.findByUserGroupId(bulkPassEntity.getUserGroupId())
+                    .stream()
+                    .map(UserGroupMappingEntity::getUserId)
+                    .collect(Collectors.toList());
+
+            count += addPasses(bulkPassEntity, userIds);
+
+            bulkPassEntity.setStatus(BulkPassStatus.COMPLETED);
+        }
+
+        log.info("AddPassesTasklet - exxcute: 이용권 {}건 추가 완료, startedAt={}", count, startedAt);
+        return RepeatStatus.FINISHED;
+    }
+
+    private int addPasses(BulkPassEntity bulkPassEntity, List<String> userIds) {
+        List<PassEntity> passEntities = new ArrayList<>();
+        for (String userId : userIds) {
+            PassEntity passEntity = PassModelMapper.INSTANCE.toPassEntity(bulkPassEntity, userId);
+            passEntities.add(passEntity);
+        }
+        return passRepository.saveAll(passEntities).size();
+    }
+}
---
+++ src/main/java/com/fastcampus/pass/repository/pass/BulkPassEntity.java
@@ -0,0 +1,35 @@
+package com.fastcampus.pass.repository.pass;
+
+import com.fastcampus.pass.repository.BaseEntity;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name = "bulk_pass")
+public class BulkPassEntity extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer bulkPassSeq;
+    private Integer packageSeq;
+    private String userGroupId;
+
+    @Enumerated(EnumType.STRING)
+    private BulkPassStatus status;
+    private Integer count;
+    private LocalDateTime startedAt;
+    private LocalDateTime endedAt;
+}
