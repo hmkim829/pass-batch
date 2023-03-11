fdec88a8e483156b771dc5f3dca9fc461703a78e
hmkim829 <hmkim829@gmail.com>
2023-03-11 19:44:29 +0900

    Batch 수업 종료 후 이용권 차감

--- src/main/java/com/fastcampus/pass/repository/booking/BookingEntity.java
+++ src/main/java/com/fastcampus/pass/repository/booking/BookingEntity.java
@@ -1,6 +1,7 @@
 package com.fastcampus.pass.repository.booking;
 
 import com.fastcampus.pass.repository.BaseEntity;
+import com.fastcampus.pass.repository.pass.PassEntity;
 import com.fastcampus.pass.repository.user.UserEntity;
 import lombok.Getter;
 import lombok.Setter;
@@ -25,7 +26,7 @@ import java.time.LocalDateTime;
 @Table(name = "booking")
 public class BookingEntity extends BaseEntity {
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // 기본 키 생성을 DB에 위임합니다. (AUTO_INCREMENT)
     private Integer bookingSeq;
     private Integer passSeq;
     private String userId;
@@ -42,4 +43,14 @@ public class BookingEntity extends BaseEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "userId", insertable = false, updatable = false)
     private UserEntity userEntity;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "passSeq", insertable = false, updatable = false)
+    private PassEntity passEntity;
+
+    // endedAt 기준, yyyy-MM-HH 00:00:00
+    public LocalDateTime getStatisticsAt() {
+        return this.endedAt.withHour(0).withMinute(0).withSecond(0).withNano(0);
+
+    }
 }
